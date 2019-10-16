679aaa0f9840d1c13c797a1a619664711c45072e
Michael Ferger <mferger92@gmail.com>
2019-10-16 15:33:07 -0600

    adds 2 modals and form to index view

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+      <img src="~/assets/images/logo.svg" alt="" />
       <h1 class="title">
         efinancial
       </h1>
@@ -20,49 +20,57 @@
           GitHub
         </a>
       </div>
+      <Modal :visible="isVisible1" @close="isVisible1 = false">
+        content
+      </Modal>
+      <Modal :visible="isVisible2" @close="isVisible2 = false" />
+      <button type="button" @click="isVisible1 = true" name="button">
+        Show
+      </button>
+      <button type="button" @click="isVisible2 = true" name="button">
+        Show 2
+      </button>
+      <form
+        id="app"
+        @submit="checkForm"
+        action="https://vuejs.org/"
+        method="post"
+        novalidate="true"
+      >
+        <p>
+          <label for="name">Name</label>
+          <input id="name" v-model="name" type="text" name="name" />
+        </p>
+        <p>
+          <label for="email">Email</label>
+          <input id="email" v-model="email" type="email" name="email" />
+        </p>
+        <p>
+          <label for="movie">Favorite Movie</label>
+          <select id="movie" v-model="movie" name="movie">
+            <option>Star Wars</option>
+            <option>Vanilla Sky</option>
+            <option>Atomic Blonde</option>
+          </select>
+        </p>
+        <p>
+          <input type="submit" value="Submit" />
+        </p>
+      </form>
     </div>
   </div>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Modal from '~/components/Modal.vue'
 export default {
+  data() {
+    return {
+      isVisible1: false,
+      isVisible2: false
+    }
+  },
   components: {
-    Logo
+    Modal
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
