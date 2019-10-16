2723ff19015922fc38b2d4187b5f6f37610c556a
Josh Horner <josh@bkmediagroup.com>
2019-10-16 10:57:45 -0600

    sets up some scss goodness

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,10 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: [
+    '@/assets/scss/global.scss'
+  ],
+  styleResources: ['@/assets/scss/vars.scss'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -40,8 +43,21 @@ export default {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources'
   ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
